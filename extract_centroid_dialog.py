379dabe3e6e6494cb03abcaa11a7979a13acd1fa
Erick Otenyo <otenyo.erick@gmail.com>
2019-02-25 17:11:32 +0300

    add validation and connect events

--- extract_centroid_dialog.py
+++ extract_centroid_dialog.py
@@ -22,14 +22,15 @@
 """
 
 import os
+from PyQt4 import uic
 
-from PyQt4 import QtGui, uic
+from PyQt4.QtGui import *
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'extract_centroid_dialog_base.ui'))
 
 
-class ExtractCentroidDialog(QtGui.QDialog, FORM_CLASS):
+class ExtractCentroidDialog(QDialog, FORM_CLASS):
     def __init__(self, parent=None):
         """Constructor."""
         super(ExtractCentroidDialog, self).__init__(parent)
@@ -40,5 +41,24 @@ class ExtractCentroidDialog(QtGui.QDialog, FORM_CLASS):
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
 
+        self.pushButton.clicked.connect(self.select_output_file)
+        self.btnBox.accepted.connect(self.accept_dialog)
 
+    def select_output_file(self):
+        filename = QFileDialog.getSaveFileName(self, "Select output file", "", "*.csv")
+        self.lineEdit.setText(filename)
 
+    def accept_dialog(self):
+        is_valid = self.validate_dialog()
+        if is_valid:
+            self.accept()
+
+    def validate_dialog(self):
+        if self.comboBox.currentIndex():
+            QMessageBox.warning(self, "Select layer", "No layer selected")
+            return False
+        if self.lineEdit.text() == "":
+            QMessageBox.warning(self, "Select output file", "Select output file")
+            return False
+        else:
+            return True
